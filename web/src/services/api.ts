69579d35f161340762d858d2b5185f3a3a8443eb
Paulo Reis <paulosilvadosreis2057@gmail.com>
2021-09-23 13:14:18 -0300

    Add refresh token logic with request queue in axios

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,11 +1,72 @@
-import axios from 'axios'
-import { parseCookies } from 'nookies'
+import axios, { AxiosError } from 'axios'
+import { setCookie, parseCookies } from 'nookies'
 
-const cookies = parseCookies()
+let cookies = parseCookies()
+let isRefreshing = false
+let failedRequestsQueue = []
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
   headers: {
     Authorization: `Bearer ${cookies['@authme.token']}`
+  }
+})
+
+api.interceptors.response.use(response => {
+  return response
+}, (error: AxiosError) => {
+  if(error.response.status === 401) {
+    if(error.response.data?.code === 'token.expired') {
+      cookies = parseCookies()
+
+      const { '@authme.refreshToken': refreshToken } = cookies
+      const originalConfig = error.config
+
+      if(!isRefreshing) {
+        isRefreshing = true
+
+        api.post('/refresh', {
+          refreshToken
+        }).then(response => {
+          const { token } = response.data
+  
+          setCookie(undefined, '@authme.token', token, {
+            maxAge: 60 * 60 * 24 * 30, // 30 days
+            path: '/'
+          })
+    
+          setCookie(undefined, '@authme.refreshToken',
+           response.data.refreshToken, {
+            maxAge: 60 * 60 * 24 * 30, // 30 days
+            path: '/'
+          })
+  
+          api.defaults.headers['Authorization'] = `Bearer ${token}}`
+
+          failedRequestsQueue.forEach(request => request.onSuccess(token))
+          failedRequestsQueue = []
+        }).catch(err => {
+          failedRequestsQueue.forEach(request => request.onFailure(err))
+          failedRequestsQueue = []
+        }).finally(() => {
+          isRefreshing = false
+        })
+      }
+
+      return new Promise((resolve, reject) => {
+        failedRequestsQueue.push({
+          onSuccess: (token: string) => {
+            api.defaults.headers['Authorization'] = `Bearer ${token}}`
+
+            resolve(api(originalConfig))
+          },
+          onFailure: (err: AxiosError) => {
+            reject(err)
+          }
+        })
+      })
+    } else {
+
+    }
   }
 })
